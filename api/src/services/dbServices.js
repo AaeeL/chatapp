6d7b664fef20bfe9dfb9ab920b79eaeac003911f
AaeeL <aaro.leikari@gmail.com>
2020-01-13 14:21:43 +0200

    removed comment

--- api/src/services/dbServices.js
+++ api/src/services/dbServices.js
@@ -13,26 +13,6 @@ const insterNewUser = async (id, username, password, created) => {
         await client.release()
         return error
     }
-    
-    /*let responseObject = {
-        success: null,
-        failure: null,
-        error: null
-    }
-    
-    try {
-        await client.query('BEGIN')
-        const query = 'INSERT INTO account(user_id, username, password, created_on) VALUES($1, $2, $3, $4) '
-        await client.query(query, [5, username, password, created])
-        await client.query('COMMIT')
-        responseObject.success = true
-    } catch (error) {
-        responseObject.failure = false
-        responseObject.error = error
-    } finally {
-        await client.release()
-        return responseObject
-    }*/
 }
 
 module.exports = insterNewUser
